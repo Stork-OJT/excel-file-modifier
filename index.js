847231b4ec4b5517ce1299486e0fef1504535434
Vihnnn <yrvihnbargola24@gmail.com>
2025-05-08 16:18:05 +0800

    Sync in excel file

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ import fs from "fs";
 
 const fileURL = new URL("./empty.xlsx", import.meta.url);
 const worksheet = xlsx.parse(fs.readFileSync(fileURL));
-console.log(fileURL);
+
 const sheet = worksheet[0].name;
-console.log(sheet);
+
 const data = [
+  ["Name", "Age"],
   ["Yrvihn Bargola", 20],
-  ["Powpow", 19],
+  ["Vinnn", 19],
 ];
 
 const buffer = xlsx.build([{ name: sheet, data: data }]);
 
-console.log(data);
+console.log(sheet);
+
+fs.writeFileSync(fileURL, buffer);
