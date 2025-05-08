620432e87da31959bf44f909d28c22c6daba36c4
Vihnnn <yrvihnbargola24@gmail.com>
2025-05-08 14:58:55 +0800

    add data

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-import fs from "node:fs/promises";
+import xlsx from "node-xlsx";
+import fs from "fs";
 
-const fileURL = new URL("../../Tinamad.xlsx", import.meta.url);
-const buffer = await fs.readFile(fileURL);
+const fileURL = new URL("./empty.xlsx", import.meta.url);
+const worksheet = xlsx.parse(fs.readFileSync(fileURL));
+console.log(fileURL);
+const sheet = worksheet[0].name;
+console.log(sheet);
+const data = [
+  ["Yrvihn Bargola", 20],
+  ["Powpow", 19],
+];
+
+const buffer = xlsx.build([{ name: sheet, data: data }]);
+
+console.log(data);
